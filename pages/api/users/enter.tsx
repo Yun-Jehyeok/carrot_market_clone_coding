0ec505d12e8ce15c47116a05580f640c3795df13
Yun-Jehyeok <dbswpgur2@naver.com>
2022-12-01 09:34:50 +0900

    Start Authentication with prisma

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,37 @@
+import client from "libs/server/client";
 import withHandler from "libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+
+  const payload = phone ? { phone: +phone } : { email };
+
+  const user = await client.user.upsert({
+    create: {
+      ...payload,
+      name: "Anonymous",
+    },
+    update: {},
+    where: {
+      ...payload,
+    },
+  });
+
+  console.log(user);
+
+  const token = await client.token.create({
+    data: {
+      payload: "aa",
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  });
+
+  console.log(token);
   return res.json({ ok: true });
 }
 
