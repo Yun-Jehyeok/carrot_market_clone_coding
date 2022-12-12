d73e20499d349c4e76996ab5ca90c1f1745c8375
Yun-Jehyeok <dbswpgur2@naver.com>
2022-12-12 16:37:28 +0900

    sendgrid & twilio

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,6 +2,12 @@ import client from "libs/server/client";
 import withHandler, { ResponseType } from "libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
+import mail from "@sendgrid/mail";
+import twilio from "twilio";
+
+mail.setApiKey(process.env.SENDGRID_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
@@ -29,6 +35,26 @@ async function handler(
       },
     },
   });
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+
+    console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "dbswpgur2@naver.com",
+      to: "cjswo35753@gmail.com",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+
+    console.log(email);
+  }
 
   return res.json({ ok: true });
 }
