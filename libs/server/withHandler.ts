9bc9d4c5d7d71d3d24bf18a8dda2e661f3eee016
Yun-Jehyeok <dbswpgur2@naver.com>
2022-12-21 09:31:02 +0900

    Start SWR & Authentication is Done!!

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -1,9 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function withHandler(
-  method: "GET" | "POST" | "DELETE",
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
-) {
+interface ConfigType {
+  method: "GET" | "POST" | "DELETE";
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
+  isPrivate?: boolean;
+}
+
+export default function withHandler({
+  method,
+  handler,
+  isPrivate = true,
+}: ConfigType) {
   return async function (
     req: NextApiRequest,
     res: NextApiResponse
@@ -12,8 +19,12 @@ export default function withHandler(
       return res.status(405).end();
     }
 
+    if (isPrivate && !req.session.user) {
+      return res.status(400).json({ ok: false, error: "Plz log in." });
+    }
+
     try {
-      await fn(req, res);
+      await handler(req, res);
     } catch (e) {
       console.log(e);
       return res.status(500).json({ e });
