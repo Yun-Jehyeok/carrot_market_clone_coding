83f4a68e4bb3bbeae52e184e7e7abd893a697b10
Yun-Jehyeok <dbswpgur2@naver.com>
2022-12-22 09:57:11 +0900

    Product Upload, List, Detail, Similars

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,43 @@
+import withHandler, { ResponseType } from "libs/server/withHandler";
+import { withApiSession } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query;
+  const product = await client?.product.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  const terms = product?.name.split(" ").map((word) => ({
+    name: {
+      contains: word,
+    },
+  }));
+  const relatedProducts = await client?.product.findMany({
+    where: {
+      OR: terms,
+      AND: {
+        id: {
+          not: product?.id,
+        },
+      },
+    },
+  });
+
+  res.json({ ok: true, product, relatedProducts });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
